080bd6d50ba4cf4260944aaede3c67535b10a5be
Alan Paul B. Paragas <alanpaulparagas@gmail.com>
2018-07-05 18:24:10 -0700

    update

--- sql5300.cpp
+++ sql5300.cpp
@@ -1,71 +1,116 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include "db_cxx.h"
 #include "SQLParser.h"
 #include "sqlhelper.h"
-#include "db_cxx.h"
 
 using namespace std;
-void initializeDBenv(char *DBenv);
+using namespace hsql;
 
-int main(int argc, char *argv[]) {
+void initializeDBenv(char *DBenv);
+string execute(const SQLStatement *query);
+
+
+//set db environement 
+void initializeDBenv(char *DBenv){
+
+    cout << "(sql5300: running with database environment at"<<DBenv<< ")"<<endl;
+
+    DbEnv myEnv(0U);
+    myEnv.set_message_stream(&cout);
+    myEnv.set_error_stream(&cerr);
+
+    //attempt to create environment 
+    try {
+        myEnv.open(DBenv, DB_CREATE | DB_INIT_MPOOL, 0);
+    } catch(DbException &e) {
+      cerr << "Error opening database environment: "
+                << envHome << std::endl;
+                cerr << e.what() << std::endl;
+      exit( -1 );
+  } catch(exception &e) {
+    std::cerr << "Error opening database environment: "
+              << envHome << std::endl;
+    std::cerr << e.what() << std::endl;
+    }
+
+}
+
+//Fr
+string createTable(const CreateStatement *stmt) {
+    string ret("CREATE TABLE ");
+    if (stmt->type != CreateStatement::kTable )
+        return ret + "...";
+    if (stmt->ifNotExists)
+        ret += "IF NOT EXISTS ";
+    ret += string(stmt->tableName) + " (";
+    bool doComma = false;
+    for (ColumnDefinition *col : *stmt->columns) {
+        if(doComma)
+            ret += ", ";
+        ret += columnDefinitionToString(col);
+        doComma = true;
+    }
+    ret += ")";
+    return ret;
+}
+
+string execute(const SQLStatement *stmt) {
+    //check the sql statment for type , TODO INSERTION 
+    switch (stmt->type()) {
+    case kStmtSelect:
+        return selectTable((const SelectStatement*) stmt);
+    case kStmtCreate:
+        return createTable((const CreateStatement*) stmt);
+    default:
+        return "No Implementation";
+    }
+} 
+
+
+int main(int argc, char **argv) {
+
+    if(argc != 2){
+        cerr<< "Error in Database Path for cpsc5300"<< endl;
+        return 1;
+    }
     //initialize environment
     initializeDBenv(argv[1]);
     //loop to get user inputs
     
+    //SQL Shell
     while(true){
         string SQLinput;
-        cout << "endl";
+        cout << endl;
         cout << "SQL>";
         getline(cin, SQLinput);
+        //not inputs
+        if (SQLinput.length() == 0){
+            continue;
+        }
         //if quit, quit
         if(SQLinput == "quit"){
             break;
         }
-        //not inputs
-        if (SQLinput.length() < 1){
-            continue;
-        }
         //hyrise parser
-        hsql::SQLParserResult *result = hsql::SQLParser::parseSQLString(SQLinput);
+        SQLParserResult *result = SQLParser::parseSQLString(SQLinput);
         //check if hyrise function is Valid, continue loop if not
         if (!result->isValid()) {
-            cout << "Invalid SQL statment";
+            cout << "Invalid SQL statment"<<endl;
             continue;
         }
         //print out sqlstatment
         else{
             for (int i = 0 ; i< result->size(); i++){
                 //hsql::SQLStatement* statement = result->getStatement(i);
-                cout<< result->getStatement(i);
+                cout<< result->getStatement(i)<<endl;
             }
 
         }
-        //reset
-        
 
     }
+    return 0;
 }
 
 
-void initializeDBenv(char *DBenv){
-    cout << "(Sql5300: running with database environment at"<<DBenv<<")";
-
-    std::string envHome(DBenv);
-    DbEnv *myEnv = new DbEnv(0U);
-
-    try {
-        myEnv->open(DBenv, DB_CREATE | DB_INIT_MPOOL, 0);
-    } catch(DbException &e) {
-        std::cerr << "Error opening database environment: "
-                  << envHome << std::endl;
-        std::cerr << e.what() << std::endl;
-        exit( -1 );
-    } catch(std::exception &e) {
-        std::cerr << "Error opening database environment: "
-                  << envHome << std::endl;
-        std::cerr << e.what() << std::endl;
-        exit( -1 );
-    }
-
-}
