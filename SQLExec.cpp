0af647be88c601e99cb6d9a534a75868256b1b88
Chun-Chi Huang <ryancc.huang0729@gmail.com>
2018-08-05 19:12:07 -0700

    add try/catch to catch DbException for DELETE

--- SQLExec.cpp
+++ SQLExec.cpp
@@ -162,40 +162,45 @@ QueryResult *SQLExec::insert(const InsertStatement *statement) {
 }
 
 QueryResult *SQLExec::del(const DeleteStatement *statement) {
-    Identifier table_name = statement->tableName;                           // get the table name from DELETE command
-    DbRelation& table = SQLExec::tables->get_table(table_name);             // get the corresponding table
-    EvalPlan *plan = new EvalPlan(table);                                   // create a new TableScan plan 
-    if (statement->expr != nullptr)                                         // if SELECT ... FROM ... WHERE
-        plan = new EvalPlan(get_where_conjunction(statement->expr), plan);  // create a new plan for SELECT
+    try {
+        Identifier table_name = statement->tableName;                           // get the table name from DELETE command
+        DbRelation& table = SQLExec::tables->get_table(table_name);             // get the corresponding table
+        EvalPlan *plan = new EvalPlan(table);                                   // create a new TableScan plan 
+        if (statement->expr != nullptr)                                         // if SELECT ... FROM ... WHERE
+            plan = new EvalPlan(get_where_conjunction(statement->expr), plan);  // create a new plan for SELECT
 
-    EvalPlan *optimized = plan->optimize();                                 // optimize the plan
-    EvalPipeline pipeline = optimized->pipeline();                          // pipeline gets handles
+        EvalPlan *optimized = plan->optimize();                                 // optimize the plan
+        EvalPipeline pipeline = optimized->pipeline();                          // pipeline gets handles
 
-    auto index_names = SQLExec::indices->get_index_names(table_name);       // get the corresponding index names
-    Handles *handles = pipeline.second;                                     // assign record ID to handles
+        auto index_names = SQLExec::indices->get_index_names(table_name);       // get the corresponding index names
+        Handles *handles = pipeline.second;                                     // assign record ID to handles
 
-    int indices_n = index_names.size();                                     // count for number of indices
-    int row_n = 0;                                                          // counter of number of del rows
+        int indices_n = index_names.size();                                     // count for number of indices
+        int row_n = 0;                                                          // counter of number of del rows
 
-    for (auto const& handle : *handles) {                                   // scan all the del rows
-        for (auto const& index_name : index_names) {                        // scan all the corresponding indices
-            DbIndex &index = SQLExec::indices->get_index(table_name, index_name);   // get index table 
-            index.del(handle);                                              // delete row's index
+        for (auto const& handle : *handles) {                                   // scan all the del rows
+            for (auto const& index_name : index_names) {                        // scan all the corresponding indices
+                DbIndex &index = SQLExec::indices->get_index(table_name, index_name);   // get index table 
+                index.del(handle);                                              // delete row's index
+            }
         }
+
+        for (auto const& handle : *handles) {                                   // scan all the del rows
+            table.del(handle);                                                  // delete row in the table
+            row_n++;                                                            // count for number of del rows
+        }
+
+        delete handles;
+
+        string suffix = "successfully deleted " + to_string(row_n) + " rows";   // construct the del message
+        if (indices_n >0)                                                       // if index exists, show index info
+            suffix += " from " + to_string(indices_n) + " indices";
+
+        return new QueryResult(suffix);
+        
+    } catch (DbException& e) {
+        throw SQLExecError(string("DbRelationError: ") + e.what());
     }
-
-    for (auto const& handle : *handles) {                                   // scan all the del rows
-        table.del(handle);                                                  // delete row in the table
-        row_n++;                                                            // count for number of del rows
-    }
-
-    delete handles;
-
-    string suffix = "successfully deleted " + to_string(row_n) + " rows";   // construct the del message
-    if (indices_n >0)                                                       // if index exists, show index info
-        suffix += " from " + to_string(indices_n) + " indices";
-
-    return new QueryResult(suffix);
 }
 
 ValueDict *SQLExec::get_where_conjunction(const Expr *whereClause) {
