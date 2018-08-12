524c2a92646e0a7377ec3791873847abfb690b43
Jared Mead <jaredgmead@gmail.com>
2018-08-11 18:57:34 -0700

    Cleaned up code and added comments

--- btree.cpp
+++ btree.cpp
@@ -10,44 +10,43 @@ BTreeIndex::BTreeIndex(DbRelation& relation, Identifier name, ColumnNames key_co
           key_profile() {
     if (!unique)
         throw DbRelationError("BTree index must have unique key");
-	// FIXME - what else?!
 	build_key_profile();
 }
 
 BTreeIndex::~BTreeIndex() {
-	// FIXME - free up stuff
 	delete(this->stat);
 	delete(this->root);
-    //	drop();
 }
 
 // Create the index.
 void BTreeIndex::create() {
-	// FIXME
 	this->file.create();
 	this->stat = new BTreeStat(this->file, this->STAT, this->STAT + 1, this->key_profile);
 	this->root = new BTreeLeaf(this->file, this->stat->get_root_id(), this->key_profile, true);
 	this->closed = false;
+
 	// now build the index! -- add every row from relation into index
 	Handles* handles = this->relation.select();
 	for (auto const& handle : *handles) {
+    // Insert row from relation into the index
 		this->insert(handle);
 	}
 }
 
 // Drop the index.
 void BTreeIndex::drop() {
-	// FIXME
+	// Drop the underlying file
 	this->file.drop();
 }
 
 // Open existing index. Enables: lookup, range, insert, delete, update.
 void BTreeIndex::open() {
-	// FIXME
 	if (this->closed) {
 		this->file.open();
 		this->stat = new BTreeStat(this->file, this->STAT, this->key_profile);
 
+    // Determine whether there is anything present in the index already and whether
+    // Root should be an interior or a leaf node
 		if (this->stat->get_height() == 1)
 			this->root = new BTreeLeaf(this->file, this->stat->get_root_id(), this->key_profile, false);
 		else
@@ -59,7 +58,6 @@ void BTreeIndex::open() {
 
 // Closes the index. Disables: lookup, range, insert, delete, update.
 void BTreeIndex::close() {
-	// FIXME
 	this->file.close();
 	this->stat = nullptr;
 	this->root = nullptr;
@@ -69,13 +67,12 @@ void BTreeIndex::close() {
 // Find all the rows whose columns are equal to key. Assumes key is a dictionary whose keys are the column
 // names in the index. Returns a list of row handles.
 Handles* BTreeIndex::lookup(ValueDict* key_dict) const {
-	// FIXME
 	KeyValue* _tKey = this->tkey(key_dict);
 	return this->_lookup(this->root, this->stat->get_height(), _tKey);
 }
 
+// Recursive helper function for lookup
 Handles* BTreeIndex::_lookup(BTreeNode *node, uint height, const KeyValue* key) const {
-	// FIXME
 	Handles* handles = new Handles();
 	Handle handle;
 	if (height == 1) {
@@ -85,42 +82,58 @@ Handles* BTreeIndex::_lookup(BTreeNode *node, uint height, const KeyValue* key) 
 		return handles;
 	}
 	else {
-		return _lookup(((BTreeInterior*)node)->find(key, this->stat->get_height()), 
+    // Search for the key in the node, use the returned node as the root of
+    // the next search
+		return _lookup(((BTreeInterior*)node)->find(key, this->stat->get_height()),
 													this->stat->get_height() - 1, key);
 	}
 }
 
+// Not Handled in Milestone 6
 Handles* BTreeIndex::range(ValueDict* min_key, ValueDict* max_key) const {
     throw DbRelationError("Don't know how to do a range query on Btree index yet");
-    // FIXME
 }
 
 // Insert a row with the given handle. Row must exist in relation already.
 void BTreeIndex::insert(Handle handle) {
-	// FIXME
 	KeyValue* _tKey = this->tkey(this->relation.project(handle, &key_columns));
 	Insertion split_root = _insert(this->root, this->stat->get_height(), _tKey, handle);
 
+  // If split root is not none, the another node needs to be added
 	if (!BTreeNode::insertion_is_none(split_root)) {
-		BTreeInterior* root = new BTreeInterior(this->file, 0, this->key_profile, true);
-		root->set_first(this->root->get_id());
+    // Create a new root, as an interior node
+    BTreeInterior* root = new BTreeInterior(this->file, 0, this->key_profile, true);
+
+    // Set the new root's id to the current root's id
+    root->set_first(this->root->get_id());
+    // Insert the values of split root into the new root
 		root->insert(&split_root.second, split_root.first);
+    // Save the new root
 		root->save();
+
+    // Transfer information of the new root into stat
 		this->stat->set_root_id(root->get_id());
 		this->stat->set_height(this->stat->get_height() + 1);
 		this->stat->save();
+
+    // Set root equal to the new root
 		this->root = root;
 	}
 }
 
+// Recursive helper function for insert
 Insertion BTreeIndex::_insert(BTreeNode *node, uint height, const KeyValue* key, Handle handle) {
-	// FIXME
 	Insertion insertion;
 	if (height == 1)
+    // If we are at the lowest level, then all that needs to be done is an insert
 		insertion = ((BTreeLeaf*)node)->insert(key, handle);
 	else {
+    // Else we need to recursively insert into the next level down
 		Insertion new_kid = _insert(((BTreeInterior*)node)->find(key, height), height - 1, key, handle);
-		if (!BTreeNode::insertion_is_none(new_kid))
+
+    // If something bubbles up, we must insert the new kid's information into
+    // the current interior node
+    if (!BTreeNode::insertion_is_none(new_kid))
 			insertion = ((BTreeInterior*)node)->insert(&new_kid.second, new_kid.first);
 	}
 	return insertion;
@@ -128,17 +141,20 @@ Insertion BTreeIndex::_insert(BTreeNode *node, uint height, const KeyValue* key,
 
 void BTreeIndex::del(Handle handle) {
     throw DbRelationError("Don't know how to delete from a BTree index yet");
-	// FIXME
 }
 
 // Transform a key dictionary into a tuple in the correct order.
 KeyValue *BTreeIndex::tkey(const ValueDict *key) const {
-	// FIXME
 	KeyValue *keyValue = new KeyValue();
+
 	for (auto const& column_name : key_columns) {
 		Value value;
-		ValueDict::const_iterator column = key->find(column_name);
-		if (column != key->end()) {
+
+    // Search for the specific column name in the key
+    ValueDict::const_iterator column = key->find(column_name);
+
+    // If the column is found, insert it into the keyValue
+    if (column != key->end()) {
 			value = column->second;
 			keyValue->push_back(value);
 		}
@@ -149,15 +165,21 @@ KeyValue *BTreeIndex::tkey(const ValueDict *key) const {
 }
 
 // build key profile
-// figure out the data types of each key component and encode them in self.key_profile, 
+// figure out the data types of each key component and encode them in self.key_profile,
 // a list of int/str classes.
 void BTreeIndex::build_key_profile() {
-	// FIXME
+
+  // Retrieve the column names and attributes from the relation
 	ColumnNames column_names = this->relation.get_column_names();
 	ColumnAttributes column_attributes = this->relation.get_column_attributes();
+
+  // Iterate through all of the columns in key_columns
 	for (auto const& key_column_name : key_columns) {
+    // Search through column names
 		for (uint i = 0; i < column_names.size(); i++) {
+      // If we find the key_column_name in column_names
 			if (key_column_name == column_names[i]) {
+        // push the attributes into key_profile from the attributes of the relation
 				this->key_profile.push_back(column_attributes[i].get_data_type());
 			}
 		}
@@ -165,7 +187,6 @@ void BTreeIndex::build_key_profile() {
 }
 
 bool test_btree() {
-	// FIXME
 	cout << "run test_btree" << endl;
 	bool result = false;
 	ColumnNames column_names;
@@ -292,7 +313,7 @@ bool test_btree() {
 	index->drop();
     delete index;
     testTable.drop();
-    
+
 
 	return result;
 }
